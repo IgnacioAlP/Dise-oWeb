9970856c0159ede9e75874a0c8cf48e1efb048e9
Ignacio Alonzo <alonzopezoi@gmail.com>
2022-11-17 13:37:45 -0500

    respaldo08

--- sesion19/agendaV3.js
+++ sesion19/agendaV3.js
@@ -1,49 +1,70 @@
-function guardarDato() {
-    const nombre = document.getElementById("nombre").value;
-    const movil = document.getElementById("movil").value;
-    const email = document.getElementById("email").value;
-    
+function guardarDato(){
+    //almacenando
+    var nombre = document.getElementById("nombre").value;
+    var movil = document.getElementById("movil").value;
+    var email = document.getElementById("email").value;
+  
     const datos = {
-        'movil': movil,
-        'email': email,      
-    };
-
+        'movil':movil,
+        'email':email,
+    }
+    //guardando datos
     localStorage.setItem(nombre, JSON.stringify(datos));
-
+  
+    //para limpiar
     document.getElementById("nombre").value = "";
     document.getElementById("movil").value = "";
     document.getElementById("email").value = "";
-    
-    actualizardatos();
-}
-
-function recuperarDato(){
-    const nombre = document.getElementById("nombre").value;
-    localStorage.setItem(nombre);
-    document.getElementById("movil").value = localStorage.getItem(nombre);
-    document.getElementById("email").value = localStorage.getItem(email);
-}
-
-function elminarDato(){
-    const nombre = document.getElementById("nombre").value;
-    localStorage.setItem(nombre);
-    actualizardatos();
-}
-
-function eliminarTodo(){
+  
+    actualizarDatos();
+  }
+  
+  function recuperarDato(){
+    var nombre = document.getElementById("nombre").value;
+  
+    //almacenando en la variable datos
+    let datos = localStorage.getItem(nombre);
+  
+    //separar
+    datos = JSON.parse(datos);
+  
+    //aparecer
+    document.getElementById("movil").value = datos.movil;
+    document.getElementById("email").value = datos.email;
+  }
+  
+  function eliminarDato(){
+    var nombre = document.getElementById("nombre").value;
+    localStorage.removeItem(nombre);
+    actualizarDatos();
+  }
+  
+  function eliminarTodo(){
     localStorage.clear();
-    actualizardatos();
-}
-
-function actualizardatos(){
+    actualizarDatos();
+  }
+  
+  function actualizarDatos(){
     var registro = "";
     if (localStorage.length === 0) {
         registro += '<li>Vacío</li>';
     } else {
-        for (var i = 0; i <= localStorage.length - 1; i++) {
-            var key = localStorage.key(i);
-            registro += '<li>' + '<span class="nom">' + key + '</span>' + '<span class="nom">' + localStorage.getItem(key) + '</span>' + '</li><br>';
-        }   
+       for (var i = 0; i <= localStorage.length - 1; i++) {
+           var key = localStorage.key(i);
+  
+           //obtengo los datos
+           let datos = localStorage.getItem(key);
+  
+           //separar
+           datos = JSON.parse(datos);
+  
+           //JSON no se trabaja con apostrofes ' sino tilde invertida `
+  
+           registro += `<li> <span class="nom"> ${key} </span> 
+           <span class="nom"> ${datos.movil} </span> 
+           <span class="nom"> ${datos.email} </span>
+           </li><br>`;
+       } 
     }
     document.getElementById('contactos').innerHTML = registro;
-}
+  }
